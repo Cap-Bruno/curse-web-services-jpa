2fda784d369afb1ce6ce6b589198a89f66f703ce
Bruno Campos <bruno.b.campos@capgemini.com>
2024-01-24 14:51:52 -0300

    Service layer, component registration

--- src/main/java/com/capgeminiAcademy/webServices/resources/UserResource.java
+++ src/main/java/com/capgeminiAcademy/webServices/resources/UserResource.java
@@ -1,18 +1,32 @@
 package com.capgeminiAcademy.webServices.resources;
 
 import com.capgeminiAcademy.webServices.entities.User;
+import com.capgeminiAcademy.webServices.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User user = new User(1L, "Bruno", "bruno@email.com", "999999", "1234");
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
     }
 }
