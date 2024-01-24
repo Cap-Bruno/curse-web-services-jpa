2bd7c2e85b7d41528c5a0ea63191321d6db4f7b6
Bruno Campos <bruno.b.campos@capgemini.com>
2024-01-24 16:12:53 -0300

    Create table order

--- src/main/java/com/capgeminiAcademy/webServices/entities/User.java
+++ src/main/java/com/capgeminiAcademy/webServices/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -19,7 +21,8 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
-
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
     public User() {
     }
 
@@ -71,6 +74,9 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
