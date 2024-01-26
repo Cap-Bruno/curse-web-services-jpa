311d3be9832e463fda1b656e5418d429e04d7b4f
Bruno Campos <bruno.b.campos@capgemini.com>
2024-01-26 16:21:10 -0300

    User update

--- src/main/java/com/capgeminiAcademy/webServices/resources/UserResource.java
+++ src/main/java/com/capgeminiAcademy/webServices/resources/UserResource.java
@@ -42,4 +42,10 @@ public class UserResource {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user){
+        user = userService.updateUser(id, user);
+        return ResponseEntity.ok().body(user);
+    }
 }
