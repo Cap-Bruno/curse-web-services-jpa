4e9ae228df9215b9934ee91a46bee77df94516b0
Bruno Campos <bruno.b.campos@capgemini.com>
2024-01-29 13:07:46 -0300

    Exception handling - delete

--- src/main/java/com/capgeminiAcademy/webServices/services/UserService.java
+++ src/main/java/com/capgeminiAcademy/webServices/services/UserService.java
@@ -2,8 +2,12 @@ package com.capgeminiAcademy.webServices.services;
 
 import com.capgeminiAcademy.webServices.entities.User;
 import com.capgeminiAcademy.webServices.repositories.UserRepository;
+import com.capgeminiAcademy.webServices.services.exceptions.DatabaseException;
 import com.capgeminiAcademy.webServices.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -30,13 +34,23 @@ public class UserService {
     }
 
     public void deleteUser(Long id){
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User updateUser(Long id, User user){
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, user);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, user);
+            return userRepository.save(entity);
+        } catch(EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
